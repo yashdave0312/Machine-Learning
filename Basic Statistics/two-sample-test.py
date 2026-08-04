e99cfe4d3801d0c913d299ce389ae848b9ef4cbe
Yash Dave <yashd6312@gmail.com>
2026-08-04 17:43:26 +0530

    two-sample test

--- Basic Statistics/two-sample-test.py
+++ Basic Statistics/two-sample-test.py
@@ -13,3 +13,26 @@ std1 = np.std(sample1,ddof = 1)
 std2 = np.std(sample2,ddof = 1)
 print("Standard deviation of sample 1:", std1)
 print("Standard deviation of sample 2:", std2)
+
+n1 = len(sample1)
+n2 = len(sample2)
+print("Sample size of sample 1:", n1)
+print("Sample size of sample 2:", n2)
+
+t_test = (mean1 - mean2) /(np.sqrt((std1**2/n1) + (std2**2/n2)))
+print("T-test statistic:", t_test)
+
+ddof1 = n1 - 1
+ddof2 = n2 - 1
+print("Degrees of freedom for sample 1:", ddof1)
+print("Degrees of freedom for sample 2:", ddof2)
+
+p_value = 2 * (1 - stats.t.cdf(abs(t_test), df=min(ddof1, ddof2)))
+print("P-value:", p_value)
+
+alpha = 0.05
+
+if p_value < alpha:
+    print("I will reject the null hypothesis.")
+else:
+    print("I will accept the null hypothesis.")
