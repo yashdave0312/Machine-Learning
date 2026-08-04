276bd5af3295aab997a0639a1005e198b434fb8f
Yash Dave <yashd6312@gmail.com>
2026-08-04 19:11:14 +0530

    values generated of chi square,p-value,degree of freedom,expected frequencies

--- Basic Statistics/chi-square_test.py
+++ Basic Statistics/chi-square_test.py
@@ -1,5 +1,16 @@
 import numpy as np
 from scipy.stats import chi2_contingency
 import seaborn as sns
+import pandas as pd
 
+df = sns.load_dataset("titanic")
+print(df)
 
+contingency_table = pd.crosstab(df['sex'],df['survived'])
+print(contingency_table)
+
+chi2, p, dof, expected = chi2_contingency(contingency_table)
+print("Chi-square statistic:", chi2)
+print("P-value:", p)
+print("Degrees of freedom:", dof)
+print("Expected frequencies:", expected)
