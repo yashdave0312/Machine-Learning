7e4be411ddc022ade5fa728cc26013326b86c763
Yash Dave <yashd6312@gmail.com>
2026-08-06 22:08:26 +0530

    Visualising

--- Project-1/insurance.py
+++ Project-1/insurance.py
@@ -1,4 +1,28 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings("ignore")
 
 df = pd.read_csv("insurance.csv")
 print(df)
+
+print(df.shape)
+print(df.info())
+print(df.describe())
+
+print(df.isnull().sum())
+
+numeric =["age", "bmi", "charges"]
+for col in numeric:
+    plt.figure(figsize=(6,4))
+    sns.histplot(df[col],kde=True,bins = 20)
+    plt.show()
+
+categorical = ["sex", "children", "smoker", "region"]
+for col in categorical:
+    plt.figure(figsize=(6,4))
+    sns.countplot(x = df[col])
+    plt.show()
