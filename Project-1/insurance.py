d31946c53670a027de748b5c5ace23557c0d974a
Yash Dave <yashd6312@gmail.com>
2026-08-07 11:23:21 +0530

    Feature extraction

--- Project-1/insurance.py
+++ Project-1/insurance.py
@@ -10,32 +10,32 @@ df = pd.read_csv("insurance.csv")
 print(df)
 
 # Exploratory Data Analysis (EDA)
-# print(df.shape)
-# print(df.info())
-# print(df.describe())
+print(df.shape)
+print(df.info())
+print(df.describe())
 
-# print(df.isnull().sum())
+print(df.isnull().sum())
 
-# numeric =["age", "bmi", "charges"]
-# for col in numeric:
-#     plt.figure(figsize=(6,4))
-#     sns.histplot(df[col],kde=True,bins = 20)
-#     plt.show()
+numeric =["age", "bmi", "charges"]
+for col in numeric:
+    plt.figure(figsize=(6,4))
+    sns.histplot(df[col],kde=True,bins = 20)
+    plt.show()
 
-# categorical = ["sex", "children", "smoker", "region"]
-# for col in categorical:
-#     plt.figure(figsize=(6,4))
-#     sns.countplot(x = df[col])
-#     plt.show()
+categorical = ["sex", "children", "smoker", "region"]
+for col in categorical:
+    plt.figure(figsize=(6,4))
+    sns.countplot(x = df[col])
+    plt.show()
 
-# for col in numeric:
-#     plt.figure(figsize=(6,4))
-#     sns.boxplot(x = df[col])
-#     plt.show()
+for col in numeric:
+    plt.figure(figsize=(6,4))
+    sns.boxplot(x = df[col])
+    plt.show()
 
-# plt.figure(figsize=(10,8))
-# sns.heatmap(df.corr(numeric_only=True), annot=True)
-# plt.show()
+plt.figure(figsize=(10,8))
+sns.heatmap(df.corr(numeric_only=True), annot=True)
+plt.show()
 
 # Data Cleaning and Preprocessing
 df_cleaned = df.copy()
@@ -65,4 +65,22 @@ print(df_cleaned.head())
 df_cleaned = df_cleaned.astype(int)
 print("Data types after conversion to int:")
 print(df_cleaned.dtypes)
+print(df_cleaned.head())
+
+#  Featture Engineering and Extraction
+
+sns.histplot(df_cleaned["bmi"],kde=True,bins =20)
+plt.show()
+
+df_cleaned["bmi_category"] = pd.cut(
+    df_cleaned["bmi"],
+    bins=[0, 18.5, 24.9, 29.9, float('inf')],
+    labels=["Underweight", "Normal weight", "Overweight", "Obesity"]
+)
+
+print(df_cleaned.head())
+df_cleaned = pd.get_dummies(df_cleaned,columns = ["bmi_category"], drop_first= True)
+print(df_cleaned.head())
+
+df_cleaned = df_cleaned.astype(int)
 print(df_cleaned.head())
