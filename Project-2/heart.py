bb9cf44d213e8183288fb40ee144d54a502d9e4d
Yash Dave <yashd6312@gmail.com>
2026-08-10 23:33:10 +0530

    replacing values with mean

--- Project-2/heart.py
+++ Project-2/heart.py
@@ -8,9 +8,27 @@ print(df.head())
 
 print(df.shape)
 print(df.describe())
-print(df.info())
+print(df.columns)
+print(df.describe())
 
 #EDA 
 
-def plots(var,num):
-    plt.subplot
+print(df.duplicated().sum())
+print(df["HeartDisease"].value_counts())
+
+print(df.isnull().sum())
+
+def plotting(var,num):
+    plt.subplot(2,2,num)
+    sns.histplot(df[var],kde=True,bins=20)
+    plt.show()
+
+plotting("Age",1)
+plotting("RestingBP",2)
+plotting("Cholesterol",3)
+plotting("MaxHR",4)
+plt.tight_layout()
+
+ch_mean = df.loc[df["Cholesterol"] != 0, "Cholesterol"].mean()
+df["Cholesterol"] = df["Cholesterol"].replace(0,ch_mean)
+print(df["Cholesterol"].describe())
