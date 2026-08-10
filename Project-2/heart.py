3431e05012d6c8c35fa01a8d8f0580bf0234a488
Yash Dave <yashd6312@gmail.com>
2026-08-10 23:56:07 +0530

    Data preprocessing

--- Project-2/heart.py
+++ Project-2/heart.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt 
+import warnings
+from sklearn.preprocessing import StandardScaler
+
+warnings.filterwarnings("ignore")
 
 df = pd.read_csv("heart.csv")
 print(df.head())
@@ -31,4 +35,31 @@ plt.tight_layout()
 
 ch_mean = df.loc[df["Cholesterol"] != 0, "Cholesterol"].mean()
 df["Cholesterol"] = df["Cholesterol"].replace(0,ch_mean)
-print(df["Cholesterol"].describe())
+print(df["Cholesterol"].head())
+
+restbp_mean = df.loc[df["RestingBP"] != 0, "RestingBP"].mean()
+df["RestingBP"] = df["RestingBP"].replace(0,restbp_mean)
+
+categorical = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"]
+
+for col in categorical:
+    plt.figure(figsize=(6,4))
+    sns.countplot(x = df[col], hue = df["HeartDisease"])
+    plt.show()
+
+sns.heatmap(df.corr(numeric_only=True), annot=True)
+plt.show()
+
+# Data  Preprocessing
+df_encoded = pd.get_dummies(df, columns=categorical, drop_first=True)
+print(df_encoded.head())
+
+df_encoded = df_encoded.astype(int)
+print(df_encoded.head())
+
+# scalarisation
+scalar = StandardScaler()
+scale_cols = ["Age","RestingBP", "Cholesterol", "MaxHR", "Oldpeak"]
+
+df_encoded[scale_cols] = scalar.fit_transform(df_encoded[scale_cols])
+print(df_encoded.head())
