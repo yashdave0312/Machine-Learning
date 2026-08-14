8363ffedf7e0b6f13f8bb082c9d06846668ef601
Yash Dave <yashd6312@gmail.com>
2026-08-14 23:11:01 +0530

    Data prepared for model training

--- Logistic_Regression_/titanic.py
+++ Logistic_Regression_/titanic.py
@@ -3,20 +3,60 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt 
 import warnings 
+from sklearn.preprocessing import StandardScaler
+from scipy.stats import chi2_contingency
 
 warnings.filterwarnings("ignore")
 
 df = sns.load_dataset("titanic")
-print(df.head())
-print(df.shape)
+# print(df.head())
+# print(df.shape)
 
-print(df.columns)
+# print(df.columns)
 
 df.drop(["who","adult_male","alive","embark_town","deck","class"],axis =1,inplace = True)
-print(df.head())
+# print(df.head())
 
-print(df.isnull().sum())
+# print(df.isnull().sum())
 
 df["age"] = df["age"].fillna(df["age"].mean(),inplace = True)
-print(df["age"].isnull().sum())
+# print(df["age"].isnull().sum())
 
+df["embarked"] = df["embarked"].fillna(df["embarked"].mode()[0],inplace = True)
+print(df.isnull().sum())
+
+# you have only two null values in the embarked column , so you can drop the rows as well
+# df.dropna(subset=["embarked"],inplace = True)
+
+print(df.head())
+
+# Data Preprocessing
+
+df["sex"] = df["sex"].map({"male":0,"female":1})
+print(df.head())
+
+print(df["embarked"].value_counts())
+df = pd.get_dummies(df,columns = ["embarked"], drop_first= True)
+print(df.head())
+
+df = df.astype("int")
+print(df.head())
+
+# Feature Scaler
+
+scaler = StandardScaler()
+scal_cols = ["pclass", "age","fare","sibsp","parch"]
+# print(df["sibsp"].value_counts())
+# print(df["parch"].value_counts())
+
+df[scal_cols] = scaler.fit_transform(df[scal_cols])
+print(df.head())
+
+sns.heatmap(df.corr(numeric_only=True),annot = True)
+plt.show()
+
+df.drop("embarked_Q", axis=1, inplace=True)
+print(df.head())
+
+final_df = df[[ "pclass","sex","age","sibsp","parch","fare","alone","embarked_S","survived"]]
+print(final_df.head())
