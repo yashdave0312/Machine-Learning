1c5e09897a881aee2271e7f6e06d1a3b1363b68c
Yash Dave <yashd6312@gmail.com>
2026-08-21 19:03:05 +0530

    implementation of grid search cv done for the knn and svm models

--- Support_Vector_Machines/iris.py
+++ Support_Vector_Machines/iris.py
@@ -3,8 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import warnings
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 warnings.filterwarnings("ignore")
 
@@ -16,7 +17,49 @@ y = df["species"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
-model_knn = KNeighborsClassifier(n_neighbors = 5)
+# KNN model testing by grid search cv
 
-model_knn(X_train,y_train)
+model_knn = KNeighborsClassifier(n_neighbors = 9)
 
+model_knn.fit(X_train,y_train)
+
+y_pred = model_knn.predict(X_test)
+print(y_test)
+print(y_pred)
+
+print("KNN score",model_knn.score(X_test,y_test))
+# now here the score is coming 100% , that means , my model is overfitiing
+# now lets use the grid search cv for knn
+
+classifier2 = GridSearchCV((model_knn),{
+    "weights" : ["uniform", "distance"],
+    "algorithm" : ["auto", "ball_tree", "kd_tree", "brute"],
+},cv= 5,return_train_score=False)
+
+classifier2.fit(X,y)
+results1 = pd.DataFrame(classifier2.cv_results_)
+print(results1[["param_weights","param_algorithm","mean_test_score"]])
+
+
+# svm model testing by grid search cv
+
+model_svm = SVC(gamma="auto")
+
+model_svm.fit(X_train,y_train)
+y_pred1 = model_svm.predict(X_test)
+print("SVM score : ",model_svm.score(X_test,y_test))
+
+# now lets use the grid search cv for svm
+
+classifier = GridSearchCV((model_svm),{
+    "C" : [1,10,20,30],
+    "kernel" : ["linear","rbf"]
+},cv= 5,return_train_score=False)
+
+print(classifier)
+
+classifier.fit(X,y)
+print(classifier.cv_results_)
+
+results = pd.DataFrame(classifier.cv_results_)
+print(results[["param_C","param_kernel","mean_test_score"]])
