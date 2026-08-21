9b36c0b5fcd564e07bc0ba8374a53053239d928d
Yash Dave <yashd6312@gmail.com>
2026-08-21 11:59:16 +0530

    Scalarisation and data cleaning

--- Project-3/customer.py
+++ Project-3/customer.py
@@ -6,3 +6,58 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
+# EDA
+df = pd.read_csv("customer.csv")
+print(df.head())
+
+print(df.columns)
+print(df.shape)
+print(df.info())
+print(df.describe())
+
+print(df.isnull().sum())
+
+df["Review Rating"] = df["Review Rating"].fillna(df["Review Rating"].mean(),inplace = True)
+print(df.isnull().sum())
+
+print(df.dtypes)
+
+df = df.drop("Customer ID",axis=1)
+print(df.head())
+
+print(df["Item Purchased"].value_counts())
+df = df.drop("Item Purchased",axis=1)
+print(df.head())
+
+numeric = []
+categorical=[]
+
+for col in df.columns:
+    if df[col].dtype == "int64":
+        numeric.append(col)
+    elif df[col].dtype == "float":
+        numeric.append(col)
+    else :
+        categorical.append(col)
+
+# for col in numeric :
+#     sns.histplot(x = df[col],kde=True)
+#     plt.show()
+
+# for col in categorical:
+#     sns.countplot(x= df[col])
+#     plt.show()
+
+# sns.heatmap(df.corr(numeric_only=True),annot=True)
+# plt.show()
+
+print(df.columns)
+# sns.heatmap(df.corr(numeric_only=True),annot=True)
+# plt.show()
+# plt.tight_layout()
+
+for col in categorical:
+    print(df[col].value_counts())
+
+df = df.drop(["Color","Location"],axis=1)
+print(df.head())
