b1b580c2cf20077fbc5a40bf3f95333bdeaddaee
Yash Dave <yashd6312@gmail.com>
2026-08-28 12:25:49 +0530

    PCA implementation

--- PCA/implementation.py
+++ PCA/implementation.py
@@ -4,3 +4,18 @@ import seaborn as sns
 from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+
+X,y = make_blobs(n_samples=500,n_features=5,centers=3,cluster_std=1.5,random_state=42)
+
+X_df = pd.DataFrame(X , columns = ["Feature 1","Feature 2","Feature 3","Feature 4","Feature 5"])
+print(X_df.head())
+
+scaler =StandardScaler()
+X_df_scaled = scaler.fit_transform(X_df)
+print(X_df_scaled)
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_df_scaled)
+print(X_pca)
+
+df_pca = pd.DataFrame(X_pca , columns = ["PC1","PC2"])
