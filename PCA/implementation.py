bb13f5321fd0c95971fe535a5380d07e44638799
Yash Dave <yashd6312@gmail.com>
2026-08-28 12:08:10 +0530

    PCA implementation

---
+++ PCA/implementation.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
