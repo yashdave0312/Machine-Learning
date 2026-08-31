30c7ed8be52ae58925cdeb3c3dee745491f5b4b1
Yash Dave <yashd6312@gmail.com>
2026-08-31 18:45:49 +0530

    Implementation of PCA done ..

--- PCA/implementation.py
+++ PCA/implementation.py
@@ -7,15 +7,21 @@ from sklearn.decomposition import PCA
 
 X,y = make_blobs(n_samples=500,n_features=5,centers=3,cluster_std=1.5,random_state=42)
 
-X_df = pd.DataFrame(X , columns = ["Feature 1","Feature 2","Feature 3","Feature 4","Feature 5"])
-print(X_df.head())
+# X_df = pd.DataFrame(X , columns = ["Feature 1","Feature 2","Feature 3","Feature 4","Feature 5"])
+# print(X_df.head())
 
 scaler =StandardScaler()
-X_df_scaled = scaler.fit_transform(X_df)
-print(X_df_scaled)
+X = scaler.fit_transform(X)
+print(X)
 
 pca = PCA(n_components=2)
-X_pca = pca.fit_transform(X_df_scaled)
-print(X_pca)
+X_pca = pca.fit_transform(X)
+print(X)
 
 df_pca = pd.DataFrame(X_pca , columns = ["PC1","PC2"])
+print(df_pca.head()) 
+
+df_pca["label"] = y
+
+sns.scatterplot(data = df_pca , x = df_pca["PC1"], y =df_pca["PC2"],hue=df_pca["label"])
+plt.show()
